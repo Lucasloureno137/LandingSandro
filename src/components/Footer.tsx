7ca94b48a51f6c4eb091705af3e066d079cd1564
Lucasloureno137 <lucasloureno137@gmail.com>
2024-11-22 18:00:21 -0300

    ENTREGA LANDING FINAL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,6 @@ const Footer: React.FC = () => {
         <p>Rua dos Advogados, 123, Cascavel - PR</p>
         <p>Telefone: (45) 9 9953-0236 | Email: contato@advocacia.com</p>
         <p>&copy; 2024 Advogados. Todos os direitos reservados. FAG.</p>
-        <a href="https://cdn.forms-content-1.sg-form.com/6816f5c0-a077-11ef-abed-9699e2e7938e" target="_blank" rel="noopener noreferrer">
-          Formulário
-        </a>
       </div>
     </footer>
   );
